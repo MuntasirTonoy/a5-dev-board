2de9d074b6a089c00d7612e4ad332cf86cbc981a
tonoy7 <tonoy947@gmail.com>
2025-03-02 22:10:57 +0600

    color increase

--- script.js
+++ script.js
@@ -42,11 +42,17 @@ document
   });
 // theme change logics
 let colors = [
+  "bg-cyan-200",
   "bg-red-200",
   "bg-green-200",
   "bg-blue-200",
   "bg-yellow-200",
   "bg-gray-200",
+  "bg-pink-200",
+  "bg-teal-200",
+  "bg-amber-200",
+  "bg-fuchsia-200",
+  "bg-rose-200",
 ];
 let index = 0;
 
